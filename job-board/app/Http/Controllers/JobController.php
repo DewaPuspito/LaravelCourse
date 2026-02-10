86c3dcfbd2d798e250ddd2da32fe9de7f561094d
DewaPuspito <dewactra02@gmail.com>
2026-02-10 14:39:36 +0700

    Employer finish

--- job-board/app/Http/Controllers/JobController.php
+++ job-board/app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
@@ -12,27 +13,12 @@ class JobController extends Controller
      */
     public function index(Request $request)
     {
+        Gate::authorize('viewAny', Job::class);
         $filters = request()->only('search', 'min_salary', 'max_salary', 'experience', 'category');
 
         return view(
             'job.index', 
-            ['jobs' => Job::with('employer')->filter($filters)->get()]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-        public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+            ['jobs' => Job::with('employer')->latest()->filter($filters)->get()]);
     }
 
     /**
@@ -40,33 +26,10 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
+        Gate::authorize('view', $job);
         return view(
             'job.show', 
             ['job' => $job->load('employer')]
         );
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
